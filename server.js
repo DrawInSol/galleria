bc16e08a46e99b064cfbd977f6e2530d4742c102
DrawInSol <drawinsol@outlook.com>
2025-04-14 14:30:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  family: 4 // Forzar IPv4
 });
 
 // Verificar conexión a la base de datos
